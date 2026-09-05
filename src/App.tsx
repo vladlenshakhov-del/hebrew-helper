8e4212cebdbccd5256f3362ca760af718576a625
vladlenshakhov-del <vladlenshakhov@gmail.com>
2026-09-05 14:27:15 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom"; // Заменили BrowserRouter на HashRouter
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/ThemeProvider";
-// Кастомный перехват touchmove отключён: PTR блокируется CSS-скроллером #root.
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-
-
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter basename="/hebrew-helper">
-
+          <HashRouter> {/* Удалили basename, он больше не нужен */}
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
@@ -33,4 +29,3 @@ const App = () => {
 };
 
 export default App;
-
