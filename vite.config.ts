3480cd3c2de4046e28d5b93bdbc7f5722b84f2c8
vladlenshakhov-del <vladlenshakhov@gmail.com>
2026-09-05 14:06:40 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
-  base: '/hebrew-helper/',
+  base: './',
   server: {
     host: "::",
     port: 8080,
