4ff6429adcb8a025220bbe214a89de096b2ddbf7
vladlenshakhov-del <vladlenshakhov@gmail.com>
2026-09-05 13:44:38 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,8 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/hebrew-helper">
+
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="*" element={<NotFound />} />
